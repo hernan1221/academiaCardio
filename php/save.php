9942ccb52f68c35d486340910962aabb78557b6d
josealez <31531140+josealez@users.noreply.github.com>
2018-04-04 09:34:36 -0500

    jose cambio 5

--- php/save.php
+++ php/save.php
@@ -3,12 +3,13 @@
 	
 	$nombre = utf8_decode($_POST["inputNombre"]);
 	$apellido = utf8_decode($_POST["inputApellido"]);
+	$correo = utf8_decode($_POST["inputCorreo"]);
 	$codigo = utf8_decode($_POST["inputCodigo"]);
     $cedula = utf8_decode($_POST["inputCedula"]);
 	$contrasena = utf8_decode($_POST["inputContraDos"]);
 
 	if( $nombre == "" || $email == "" || $contrasena == ""){
 	}else{
-		$insert = mysqli_query($con,'INSERT INTO ac_form(nombres,apellidos, cedula, codigo,contrasena) VALUES ("'.$nombre.'","'.$apellido.'","'.$cedula.'","'.$codigo.'","'.$contrasena.'")')or die("no insertó : ". mysql_error());
+		$insert = mysqli_query($con,'INSERT INTO ac_form(nombres,apellidos,correo,codigo,cedula,contrasena) VALUES ("'.$nombre.'","'.$apellido.'","'.$correo.'","'.$codigo.'","'.$cedula.'","'.$contrasena.'")')or die("no insertó : ". mysql_error());
 	}	
 ?>
